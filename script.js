49da2a06261fb4904820c83338da8d68ab8f8e59
Skrolled2023 <145796605+Skrolled2023@users.noreply.github.com>
2023-11-24 12:35:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// let currentPdfIndex = 1;
+// const pdfFiles = ['./ASSETS/Home_compressed.pdf', './ASSETS/About_compressed.pdf', './ASSETS/Curriculum_compressed.pdf', './ASSETS/Contact Us.pdf'];
+
+// function updatePdf(direction) {
+//     if (direction === 'next') {
+//         currentPdfIndex = (currentPdfIndex + 1) % pdfFiles.length;
+//     } else if (direction === 'prev') {
+//         currentPdfIndex = (currentPdfIndex - 1 + pdfFiles.length) % pdfFiles.length;
+//     }
+//     document.getElementById('pdfFrame').src = pdfFiles[currentPdfIndex];
+// }
+
+// document.getElementById('prevPdf').addEventListener('click', function() {
+//     updatePdf('prev');
+// });
+
+// document.getElementById('nextPdf').addEventListener('click', function() {
+//     updatePdf('next');
+// });
+
+let currentPdfIndex = 1;
+const pdfFiles = ['./ASSETS/Home_compressed.pdf', './ASSETS/About_compressed.pdf', './ASSETS/Curriculum_compressed.pdf', './ASSETS/Contact Us.pdf'];
+
+function updatePdf(direction) {
+    if (direction === 'next') {
+        currentPdfIndex = (currentPdfIndex + 1) % pdfFiles.length;
+    } else if (direction === 'prev') {
+        currentPdfIndex = (currentPdfIndex - 1 + pdfFiles.length) % pdfFiles.length;
+    }
+    let pdfUrl = pdfFiles[currentPdfIndex] + "#toolbar=0&navpanes=0&scrollbar=0";
+    document.getElementById('pdfFrame').src = pdfUrl;
+}
+
+document.getElementById('prevPdf').addEventListener('click', function() {
+    updatePdf('prev');
+});
+
+document.getElementById('nextPdf').addEventListener('click', function() {
+    updatePdf('next');
+});
+
+// Initial load
+document.getElementById('pdfFrame').src = pdfFiles[0] + "#toolbar=0&navpanes=0&scrollbar=0";
