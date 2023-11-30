7594ad42127ca2d1328ceb6024dd2edaa3af71a0
Skrolled2023 <145796605+Skrolled2023@users.noreply.github.com>
2023-11-30 15:31:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// let currentPdfIndex = 1;
-// const pdfFiles = ['./ASSETS/Home_compressed.pdf', './ASSETS/About_compressed.pdf', './ASSETS/Curriculum_compressed.pdf', './ASSETS/Contact Us.pdf'];
-
-// function updatePdf(direction) {
-//     if (direction === 'next') {
-//         currentPdfIndex = (currentPdfIndex + 1) % pdfFiles.length;
-//     } else if (direction === 'prev') {
-//         currentPdfIndex = (currentPdfIndex - 1 + pdfFiles.length) % pdfFiles.length;
-//     }
-//     document.getElementById('pdfFrame').src = pdfFiles[currentPdfIndex];
-// }
-
-// document.getElementById('prevPdf').addEventListener('click', function() {
-//     updatePdf('prev');
-// });
-
-// document.getElementById('nextPdf').addEventListener('click', function() {
-//     updatePdf('next');
-// });
 
 let currentPdfIndex = 1;
-const pdfFiles = ['./ASSETS/Home_compressed.pdf', './ASSETS/About_compressed.pdf', './ASSETS/Curriculum_compressed.pdf', './ASSETS/Contact Us.pdf'];
+const pdfFiles = ['./ASSETS/ASSETS/Home_compressed_compressed.pdf', './ASSETS/About_compressed.pdf', './ASSETS/Curriculum_compressed.pdf', './ASSETS/Contact Us.pdf'];
 
 function updatePdf(direction) {
     if (direction === 'next') {
@@ -41,3 +22,4 @@ document.getElementById('nextPdf').addEventListener('click', function() {
 
 // Initial load
 document.getElementById('pdfFrame').src = pdfFiles[0] + "#toolbar=0&navpanes=0&scrollbar=0";
+//JEHER KAAM H BHAI MT KRO
